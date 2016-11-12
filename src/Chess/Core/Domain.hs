f64ea684c8215f558cc2aa03c4ea2a5639ad1856
Kyle Lomeli <kyle@cargurus.com>
2016-11-12 10:32:41 -0500

    Fleshed out types

--- src/Chess/Core/Domain.hs
+++ src/Chess/Core/Domain.hs
@@ -1,7 +1,38 @@
 module Chess.Core.Domain where
 
-data Peice = Pawn | Rook | Knight deriving Show
+data Color = Black | White
+    deriving Show
 
-data Player = Human | Computer deriving Show
+data PieceType = Pawn | Rook | Knight | Bishop | King | Queen
+    deriving Show
 
+data Piece = Piece { color     :: Color
+                   , pieceType :: PieceType
+                   }
+    deriving Show
 
+data Player = Human String
+              | Computer String
+    deriving Show
+
+data Coordinate = Coordinate Int Int
+    deriving Show
+
+data Space = Space { piece :: Maybe Piece
+                   , color :: Color
+                   , coord :: Coordinate
+                   }
+           | Void Coordinate
+    deriving Show
+
+type Board = [Space]
+
+data Move = Move { piece    :: Piece
+                 , position :: Coordinate
+                 }
+    deriving Show
+
+data GameState = GameState { board :: Board
+                           , moves :: [Move]
+                           }
+    deriving Show
