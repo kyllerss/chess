f7b834fc02590edfb3801911b2bd9a88f530bbdb
klomeli <klomeli@gmail.com>
2017-02-12 20:41:01 -0500

    Removed reference 'Comment' code, added 'NewGame' handler.

---
+++ Handler/NewGame.hs
@@ -0,0 +1,6 @@
+module Handler.NewGame where
+
+import Import
+
+getNewGameR :: Handler Html
+getNewGameR = error "Not yet implemented: getNewGameR"
---
+++ test/Handler/NewGameSpec.hs
@@ -0,0 +1,10 @@
+module Handler.NewGameSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getNewGameR" $ do
+        error "Spec not implemented: getNewGameR"
+
