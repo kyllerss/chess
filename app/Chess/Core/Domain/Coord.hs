25ef08c04cf5d3a6d5cad9ed4c0a7998c1b73725
klomeli <klomeli@gmail.com>
2017-02-26 14:18:27 -0500

    Added JSON decoder for Coord

--- app/Chess/Core/Domain/Coord.hs
+++ app/Chess/Core/Domain/Coord.hs
@@ -8,6 +8,17 @@ data Coord = Coord Int Int
 instance ToJSON Coord where
     toJSON (Coord x y) = toJSON $ [ x, y ]
 
+instance FromJSON Coord where
+  parseJSON value = do
+    [x, y] <- parseJSON value
+    return $ Coord x y
+
+{-
+import Data.Aeson
+let c = encode $ Coord 1 2
+(decode c) :: Maybe Coord
+-}
+
 instance Ord Coord where
     (Coord x1 y1) `compare` (Coord x2 y2) =
         (show x1 ++ " -- " ++ show y1) `compare` (show x2 ++ " -- " ++ show y2)
